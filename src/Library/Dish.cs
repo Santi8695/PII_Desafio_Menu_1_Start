45037943fb8fda21c83ec4d200021fe92b555ec9
Manuel Perdomo <manuelperdomo2908@gmail.com>
2026-08-19 09:32:37 -0300

    Dish3

--- src/Library/Dish.cs
+++ src/Library/Dish.cs
@@ -12,22 +12,40 @@ namespace Ucu.Poo.Restaurant
     public class Dish
     {
         private string name;
+        /// <summary>
+        /// Representa el nombre del plato.
+        /// </summary>
         public string Name
         {
             get {return name;}
             set {name = value;}
         }
-        private float price;
-        public float Price
+        private double price;
+        /// <summary>
+        /// Representa un el precio del plato.
+        /// </summary>
+        public double Price
         {
             get {return price;}
             set {price = value;}
         }
         private bool isVegetarian;
+        /// <summary>
+        /// Representa si el plato es vegetariano o no.
+        /// </summary>
         public bool IsVegetarian
         {
             get {return isVegetarian;}
             set {isVegetarian = value;}
+        }
+        /// <summary>
+        /// El constructor.
+        /// </summary>
+        public Dish(string name, double price, bool isVegetarian)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.isVegetarian = IsVegetarian;
         }
     }
 }
