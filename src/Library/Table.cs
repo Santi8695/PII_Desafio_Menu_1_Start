a02225bd462adbaa534925defe74a4ab1abf898b
¨MICHEL-BOLSO¨ <michelaabbona@gmail.com>
2026-08-19 10:31:52 -0300

    desarrollo_mesa

--- src/Library/Table.cs
+++ src/Library/Table.cs
@@ -8,33 +8,41 @@ using System.Collections;
 
 namespace Ucu.Poo.Restaurant
 {
-    /// <summary>
-    /// Representa una mesa en el restaurante.
-    /// </summary>
     public class Table
     {
         private ArrayList order = new ArrayList();
-        public int Number { get}
+
+        public int Number { get; }
+
         public bool IsOccupied { get; private set; }
-        public arrayList Order { get { return this.order; } } 
+
+        public ArrayList Order
+        {
+            get { return this.order; }
+        }
+
         public Table(int number)
         {
             this.Number = number;
             this.IsOccupied = false;
         }
+
         public void Occupy()
         {
             this.IsOccupied = true;
         }
-        public void free()
+
+        public void Free()
         {
             this.IsOccupied = false;
             this.order.Clear();
         }
-        public void AddOrder(Dish dish)
+
+        public void AddToOrder(Dish dish)
         {
             this.order.Add(dish);
         }
+
         public bool HasOrders()
         {
             return this.order.Count > 0;
