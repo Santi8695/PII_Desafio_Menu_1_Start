0aa4cb5978755f4f4189829ec0bb67c12f20fac0
¨MICHEL-BOLSO¨ <michelaabbona@gmail.com>
2026-08-19 09:13:16 -0300

    Implementacion de la clase mesa

--- src/Library/Table.cs
+++ src/Library/Table.cs
@@ -14,7 +14,27 @@ namespace Ucu.Poo.Restaurant
     public class Table
     {
         private ArrayList order = new ArrayList();
-
+        public int Number { get}
+        public bool IsOccupied { get; private set; }
+        public arrayList Order { get { return this.order; } } 
+        public Table(int number)
+        {
+            this.Number = number;
+            this.IsOccupied = false;
+        }
+        public void Occupy()
+        {
+            this.IsOccupied = true;
+        }
+        public void free()
+        {
+            this.IsOccupied = false;
+            this.order.Clear();
+        }
+        public void AddOrder(Dish dish)
+        {
+            this.order.Add(dish);
+        }
         public bool HasOrders()
         {
             return this.order.Count > 0;
